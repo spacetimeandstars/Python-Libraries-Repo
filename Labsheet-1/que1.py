fe4d75e5dd3b3466f5e8a3391126c02e7aa8e5f2
aman tiwari <amnntiwari10@gmail.com>
2026-02-16 06:54:43 +0530

    solve que1

---
+++ Labsheet-1/que1.py
@@ -0,0 +1,11 @@
+# a) a) Import the NumPy library.
+
+import numpy as np
+
+# b) Create a 1D array with values ranging from 0 to 19.
+
+arr = np.arange(20)
+
+# c) Identify the data type and shape of the array.
+
+print("Data type:", arr.dtype)
